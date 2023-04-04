fa4ae50fd63096c55e78e24fb822c65760888a79
Aryan Aithal <75765381+aryanaithal12@users.noreply.github.com>
2023-04-04 23:20:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
-from tkinter import *
 import requests
+from datetime import datetime
+
+MY_LAT = 12.932892  # Your latitude
+MY_LONG = 77.571201  # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
 
 
-def get_quote():
-    req = requests.get(url="https://api.kanye.rest/")
-    req.raise_for_status()
-    quote = req.json()["quote"]
-    canvas.itemconfig(quote_text, text=quote)
-    return quote
-    # Write your code here.
+# Your position is within +5 or -5 degrees of the ISS position.
+def is_overhead():
+    global MY_LAT, MY_LONG, iss_longitude, iss_longitude
+    if (MY_LONG - 5.0 < iss_longitude < MY_LONG + 5.0) and (MY_LAT - 5 < iss_latitude < MY_LAT + 5):
+        return True
+    return False
 
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+def is_dark():
+    global time_now, sunset, sunrise
+    if time_now.hour > sunset or time_now.hour < sunset:
+        return True
+    return False
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="", width=250, font=("Arial", 30, "bold"),
-                                fill="white")
-canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
 
-window.mainloop()
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data2 = response.json()
+print(data2)
+sunrise = int(data2["results"]["sunrise"].split("T")[1].split(":")[0]) + 5
+sunset = int(data2["results"]["sunset"].split("T")[1].split(":")[0]) + 5
+print(sunset, sunrise)
+time_now = datetime.now()
+print(time_now)
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
